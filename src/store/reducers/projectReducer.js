e7433d49117e1922dfbd32926f12af9105353ea9
Okamoto Yuusuke <yuusuke_mbp@OkamotonoMacBook-Pro.local>
2020-04-29 16:08:35 +0900

    Delete Data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,11 +1,6 @@
 const initState = {
     projects: [
-        {id: 1, title: 'Hello React', content: "blah blah blah"},     
-        {id: 2, title: 'Hello React', content: "blah blah blah"},
-        {id: 3, title: 'Hello React', content: "blah blah blah"},     
-        {id: 4, title: 'Hello React', content: "blah blah blah"},
-        {id: 5, title: 'Hello React', content: "blah blah blah"},     
-        {id: 6, title: 'Hello React', content: "blah blah blah"},
+        
     ]
     
 }
